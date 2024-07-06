1c4656656c4d3532c685e61c23bb7f047c137e1f
nhemgomia <gomianehemiahrenel@gmail.com>
2024-07-06 13:04:45 +0800

    Add verify email and seed initial user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'zEries',
+            'email' => 'keqingaquilazhong@gmail.com',
+            'password' => bcrypt('Admin123'),
+            'email_verified_at' => time()
         ]);
     }
 }
